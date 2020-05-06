2b0ec943337115525059c264a95d824d64c4147d
P Sinha <hiitech.ml@gmail.com>
2020-05-06 16:53:07 +0530

    DMer.py

---
+++ DMer.py
@@ -0,0 +1,101 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+import time
+import schedule
+
+
+x = 0
+
+def DMer():
+    usrnames = ['psinha_09', 'sinha.py']
+
+    chrome_options = Options()
+    chrome_options.add_argument(
+        '--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1')
+    browser = webdriver.Chrome(options=chrome_options)
+    browser.get('https://www.instagram.com/accounts/login/')
+
+    time.sleep(2)
+
+    usrname_bar = browser.find_element_by_name('username')  # Find the username bar
+    passwrd_bar = browser.find_element_by_name('password')  # Find the password bar
+
+    username = 'py_test'  # Enter your username here
+    password = 'qwertypad'  # Enter your password here
+
+    usrname_bar.send_keys(username)
+    passwrd_bar.send_keys(password + Keys.ENTER)
+
+    time.sleep(11)
+
+
+    def send_msg(usrnames):
+        browser.get('https://www.instagram.com/direct/new/')
+
+        time.sleep(5)
+
+        to_btn = browser.find_element_by_name('queryBox')
+        to_btn.send_keys(usrnames)
+
+        time.sleep(8)
+
+        chk_mrk = browser.find_element_by_class_name('dCJp8')
+        chk_mrk.click()
+
+        time.sleep(3)
+
+        nxt_btn = browser.find_element_by_xpath('//div[@class="mXkkY KDuQp"]')
+        nxt_btn.click()
+
+        time.sleep(6)
+
+        txt_box = browser.find_element_by_tag_name('textarea')
+        txt_box.send_keys(f"Hi @{usrnames} ! What's up ?")
+
+        time.sleep(2)
+
+        snd_btn = browser.find_elements_by_tag_name('button')
+        snd_btnn = snd_btn.__getitem__(3)
+        snd_btnn.click()
+
+        time.sleep(4)
+
+
+    count = 0
+    try:
+        for usrnamee in usrnames:
+            send_msg(usrnamee)
+            count += 1
+
+    except TypeError:
+        print('Failed!')
+
+    browser.quit()
+
+    print(f'''
+    Successfully Sent {count} Massages
+    
+    Python Programme by
+          ___   ___ _      _         
+         | _ \ / __(_)_ _ | |_  __ _ 
+         |  _/ \__ \ | ' \| ' \/ _` |
+         |_|   |___/_|_||_|_||_\__,_|
+    
+        Follow me on Instagram @sinha.py 
+    
+        Github @sinhapy
+    ''')
+    
+    x += 1
+
+timee = "16:44"  # Specific Time When The Post will be Posted
+
+schedule.every().day.at(timee).do(DMer)
+
+try:
+    while True and x != 1:
+        schedule.run_pending()
+        time.sleep(1)
+except UnboundLocalError:
+    print('')
